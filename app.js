7d596df71ac03ea2ddd5bd5d87b1b23bf169b716
Luis Mauricio Silva da Cruz <luismauriciosilvadacruz2@gmail.com>
2024-02-23 14:37:36 -0300

    determinando uma função
    aprendendo a criar função

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-//criando variavel titulo com comando 'document.querySelector' para manipular o h1 do documento HTML.
+//criando variavel titulo com comando 'document.querySelector' para manipular o "h1" do documento HTML.
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do Numero Secreto';
 
+//criando uma variavel paragrafo com comando 'document.querySelector' para manipular o "p" do documento HTML.
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+function verificarChute () {
+    
+}
