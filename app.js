f06c17c185bf80f0c8d297ae6e3db514d4f476c4
Luis Mauricio Silva da Cruz <luismauriciosilvadacruz2@gmail.com>
2024-02-24 20:20:58 -0300

    função com return
    aprendendo a usar return numa função para armazenar numa variavel

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 //criando variavel titulo com comando 'document.querySelector' para manipular o "h1" do documento HTML.
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Numero Secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do Numero Secreto';
 
 //criando uma variavel paragrafo com comando 'document.querySelector' para manipular o "p" do documento HTML.
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
 
-function verificarChute () {
-    console.log ('clicou')
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+// essa função faz tudo que foi feito no codigo acima comentado com o codigo otimizado
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
+
+exibirTextoNaTela('h1','Jogo do Numero Secreto');
+exibirTextoNaTela('p','Escolha um numero entre 1 e 10');
+
+//função gera um numero aleatorio e usar a palavra reservada return para conseguirmos mantela armazenada na variavel
+function gerarNumeroAleatorio() {
+    return parseInt(math.ramdom() * 10 + 1);
+}
+
+
+function verificarChute() {
+    console.log (numeroSecreto);
+}
+
